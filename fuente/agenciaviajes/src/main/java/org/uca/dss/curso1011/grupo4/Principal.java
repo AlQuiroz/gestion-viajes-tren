9c6a9f9c79a33ac02404bf4fbc777db64e24a8d1
manuel.brihuega <manuel.brihuega@26848040-be99-1a6a-9449-019f22d8b209>
2011-04-05 14:35:26 +0000

    Creada la jerarquia de directorios

---
+++ fuente/agenciaviajes/src/main/java/org/uca/dss/curso1011/grupo4/Principal.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.uca.dss.curso1011.grupo4;
+
+/**
+ *
+ * @author manuel
+ */
+public class Principal {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hola");
+    }
+
+}
