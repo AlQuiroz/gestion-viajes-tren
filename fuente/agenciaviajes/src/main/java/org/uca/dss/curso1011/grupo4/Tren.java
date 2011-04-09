1971e6efff5ffcda6e890f436dcc416692251d2f
mapm1988@gmail.com <mapm1988@gmail.com@26848040-be99-1a6a-9449-019f22d8b209>
2011-04-09 17:27:51 +0000

    Creada la clase tren

---
+++ fuente/agenciaviajes/src/main/java/org/uca/dss/curso1011/grupo4/Tren.java
@@ -0,0 +1,30 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.uca.dss.curso1011.grupo4;
+
+/**
+ * Clase que representa los trenes del sistema.
+ *
+ * Son los trenes que realizaran los viajes de nuestra aplicación.
+ *
+ * @author migue
+ */
+public class Tren {
+    public Tren(int asientos, float costetramo, String tipo, String nombre){
+        this._numasiento = asientos;
+        this._costetramo = costetramo;
+        this._tipo = tipo;
+        this._nombre = nombre;
+    };
+
+    private
+
+    int _numasiento;
+    float _costetramo;
+    String _tipo;
+    String _nombre;
+
+}
