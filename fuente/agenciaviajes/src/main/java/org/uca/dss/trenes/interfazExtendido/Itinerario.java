bd2ccc9d34b1800d77de8a173d23867369d30a82
mapm1988@gmail.com <mapm1988@gmail.com@26848040-be99-1a6a-9449-019f22d8b209>
2011-05-25 17:31:13 +0000

    Los Interfaz de la 4º entrega

---
+++ fuente/agenciaviajes/src/main/java/org/uca/dss/trenes/interfazExtendido/Itinerario.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.uca.dss.trenes.interfazExtendido;
+
+import java.util.List;
+
+/**
+ * Interfaz de Itinerario, se define por comodidad para el resto.
+ * @author dmolina
+ */
+public interface Itinerario extends List<InformacionTrayecto> {
+    public double getPrecio();
+}
