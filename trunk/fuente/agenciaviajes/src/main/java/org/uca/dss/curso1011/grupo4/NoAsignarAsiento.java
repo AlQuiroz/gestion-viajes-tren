a08f79ea112239b47de585f972a40a5f7a14588f
manuel.brihuega <manuel.brihuega@26848040-be99-1a6a-9449-019f22d8b209>
2011-06-03 10:05:36 +0000

    Terminando de comentar código

--- trunk/fuente/agenciaviajes/src/main/java/org/uca/dss/curso1011/grupo4/NoAsignarAsiento.java
+++ trunk/fuente/agenciaviajes/src/main/java/org/uca/dss/curso1011/grupo4/NoAsignarAsiento.java
@@ -9,18 +9,20 @@ import org.joda.time.LocalDate;
 import org.joda.time.LocalTime;
 import org.uca.dss.trenes.interfazExtendido.InterfazRepartoAsiento;
 /**
+ * Clase encargada de implementar la interfaz 'InterfazRepartoAsiento'.
+ *
+ * Se encarga de repartir los asientos sin ninguna asignación, implementando el único método de la interfaz.
  *
  * @author migue
  */
 public class NoAsignarAsiento implements InterfazRepartoAsiento{
 
     public int reparteAsiento(GestionReservas info,String origen, String destino,LocalDate fecha, LocalTime hora){
+        /*Lanzamos una excepción si no existen asientos libres.*/
         if (info.asientosLibres(origen,destino,fecha,hora) < 0){
             throw new RuntimeException("No quedan asientos disponibles");
         }
+        /* Devolvemos -1, que será el número de los asientos a los que no se asigna un tipo de reparto.*/
         return -1;
     }
-
-
-
 }
